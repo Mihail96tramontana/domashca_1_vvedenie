819ae34f22c35fee9f8ef06b58014cf4d4ff6c30
parnishca <xelysqgbhju@gmail.com>
2022-05-25 13:12:13 +0300

    first commit

---
+++ src/test/java/guru/qa/FirstTest.java
@@ -0,0 +1,11 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+    @Test
+    void FirstTest() {
+        Assertions.assertTrue( 3 > 2);
+    }
+}
